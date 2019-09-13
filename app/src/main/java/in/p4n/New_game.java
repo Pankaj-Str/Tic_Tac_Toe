adb88841096aef6f5fcc145da8fffae40b45a6bc
p4ntechnology <pankajchouhan80@gmail.com>
2019-09-13 16:06:00 +0530

    add new game spin bottal

---
+++ app/src/main/java/in/p4n/New_game.java
@@ -0,0 +1,14 @@
+package in.p4n;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class New_game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+    }
+}
